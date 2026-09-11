06487e12fd4a1e6a616e12656e86008e7d2c2574
petero-dk <2478689+petero-dk@users.noreply.github.com>
2026-09-11 13:16:10 -0700

    Fix linting errors

--- internal/provider/client.go
+++ internal/provider/client.go
@@ -38,7 +38,7 @@ func (c *Client) request(ctx context.Context, method, path string, body []byte) 
 	if err != nil {
 		return nil, err
 	}
-	defer resp.Body.Close()
+	defer func() { _ = resp.Body.Close() }()
 
 	respBody, err := io.ReadAll(resp.Body)
 	if err != nil {
--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	"github.com/plain-insure/terraform-provider-herodesk/internal/provider"
 )
 
-var version = "dev"
-
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: func() *schema.Provider {
